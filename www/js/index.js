718265c702f6af6ef8f23ef991dfcd126e952e80
chrisbowers <chris.p.bowers@gmail.com>
2014-10-09 13:50:17 +0100

    handled lifecycle events
    modified phonegap-start project to address all application life events
    (resume, pause).

---
+++ www/js/index.js
@@ -0,0 +1,58 @@
+var app = {
+    
+	// on initialise set up listebers for application lifectycle events
+    initialize: function() {
+      	document.addEventListener('deviceready', this.onDeviceReady, false);
+		document.addEventListener("pause", this.onPause, false);
+		document.addEventListener("resume", this.onResume, false);
+    },
+	
+    // deviceready Event Handler
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+	
+	 // deviceready Event Handler
+    onResume: function() {
+        app.receivedEvent('resume');
+    },
+	
+	 // deviceready Event Handler
+    onPause: function() {
+        app.receivedEvent('pause');
+    },
+	
+	
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        
+		var parentElement = document.getElementById(id);
+        
+		var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+		var resumeElement = parentElement.querySelector('.resumed');
+        var pausedElement = parentElement.querySelector('.paused');
+
+		listeningElement.setAttribute('style', 'display:none;');
+		receivedElement.setAttribute('style', 'display:none;');
+		resumeElement.setAttribute('style', 'display:none;');
+        pausedElement.setAttribute('style', 'display:none;');
+		
+		switch (id) {
+			
+			case "deviceready": 
+				receivedElement.setAttribute('style', 'display:block;');
+				break;
+			
+			case "resume":
+				resumeElement.setAttribute('style', 'display:block;');
+				break;
+			
+			case "pause":
+				pausedElement.setAttribute('style', 'display:block;');
+				break;
+		}
+		
+        console.log('Received Event: ' + id);
+    }
+};
